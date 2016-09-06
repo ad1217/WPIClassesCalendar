0329e23c2df11745ca0652760c945432a1fad655
Adam Goldsmith <adam@adamgoldsmith.name>
2016-09-06 14:45:45 -0400

    Better parsing of class data

--- WPIClassesCalendar.py
+++ WPIClassesCalendar.py
@@ -53,7 +53,11 @@ def parse_classes(text):
     for index, class_table in enumerate(tables):
         if class_table.caption.text == "Scheduled Meeting Times":
             continue
-        class_col = class_table.find_all(attrs={'class':"dddefault"})
+
+        # parse data into dict
+        class_col = class_table.find_all('tr')
+        class_col_data = {tr.th.text.replace(':', ''): tr.td.text.strip() for tr in class_col}
+
         caption = class_table.caption.text.split(' - ')
         for row in tables[index + 1].find_all('tr'):
             time_col = row.find_all('td', attrs={'class':"dddefault"})
@@ -63,9 +67,11 @@ def parse_classes(text):
             class_data = {'title'             : caption[0],
                           'course'            : caption[1],
                           'section'           : caption[2],
-                          'CRN'               : class_col[1].text,
-                          'status'            : class_col[2].text,
-                          'course_instructor' : class_col[3].text.replace('\n', ''),
+                          'term'              : class_col_data['Associated Term'],
+                          'Status'            : class_col_data['Status'],
+                          'CRN'               : class_col_data['CRN'],
+                          'course_instructor' : class_col_data['Assigned Instructor'],
+                          'waitlist_position' : class_col_data.get('Waitlist Position'),
                           'times'             : time_col[1].text.split(" - "),
                           'days'              : time_col[2].text,
                           'location'          : time_col[3].text,
